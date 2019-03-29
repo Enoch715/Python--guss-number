50d08f195bf6b23798992a332074796f77040ed2
CHENGHSIEN Lo <aszx99370765@gmail.com>
2019-03-29 16:49:58 +0800

    second version--add user input start & end

--- gussnumber.py
+++ gussnumber.py
@@ -1,11 +1,13 @@
 import random
-num = random.randint(0, 100) #產生隨機數字
+start = int(input('請決定隨機數字開始值: '))
+end = int(input('請決定隨機數字結束值: '))
+num = random.randint(start, end) #產生隨機數字
 count = 0
 while True:
-	user = int(input('請猜數字(1-100): '))
+	user = int(input('請猜數字: '))
 	count += 1
 	if user == num:
-		print ('恭喜猜對了!')
+		print ('恭喜猜對了!!!!共猜測', count, '次')
 		break
 	elif user > num:
 		print('比答案大')
